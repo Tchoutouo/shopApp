2b931782e2fa504b09459589d98b737a076e2f2f
bricetangang <brice.tangang@dembou-consulting.com>
2023-12-21 09:16:59 +0100

    ajustement du footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  
+  siteName: string = environment.siteName
+
+  constructor(){}
+  
+  ngOnInit(): void {
+  }
 
 }
